158689a74aef96c0a6cb73093a968f1954e6741a
baphi404 <philippbasler1@gmx.de>
2023-03-21 21:41:27 +0100

    impl backend and frontend

---
+++ movies/src/main/java/learn/movies/review/ReviewService.java
@@ -0,0 +1,30 @@
+package learn.movies.review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import learn.movies.movie.Movie;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    public ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
